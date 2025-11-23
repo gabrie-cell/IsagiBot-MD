6d63c81cd73dbaa6557974e4c7de29120620a7bb
gabrie-cell <doxeosjr@gmail.com>
2025-11-23 06:52:47 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -4,12 +4,12 @@ VERSION:3.0
 FN:𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
 ORG:𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
 TITLE:Epictetus, Enchiridion — Chapter 1 (verse 1)
-EMAIL;type=INTERNET:fedelanyt20@gmail.com 
-TEL;type=CELL;waid=5491124918653:+5491124918653
+EMAIL;type=INTERNET:doxeosjr@gmail.com 
+TEL;type=CELL;waid=51941247696:+51941247696
 ADR;type=WORK:;;2-chōme-7-5 Fuchūchō;Izumi;Osaka;594-0071;Japan
-URL;type=WORK:https://www.instagram.com/dev-fedexyz13
-X-WA-BIZ-NAME:Ｓｈａｄｏｗ - Ｂｏｔ
-X-WA-BIZ-DESCRIPTION:🄲 𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘣𝘺 𝘋𝘦𝘷-𝘧𝘦𝘥𝘦𝘹𝘺𝘻
+URL;type=WORK:https://www.instagram.com/DV G Dx
+X-WA-BIZ-NAME:I S A G I - Ｂｏｔ
+X-WA-BIZ-DESCRIPTION:🄲 𝘗𝘰𝘸𝘦𝘳𝘦𝘥 𝘣𝘺 𝘋𝘦𝘷-𝘧Dani
 X-WA-BIZ-HOURS:Mo-Su 00:00-23:59
 END:VCARD`;
 
@@ -36,8 +36,8 @@ END:VCARD`;
             },
             contextInfo: {
                 externalAdReply: {
-                    title: "© 2024–2025 Shadow Project",
-                    body: "Contacta con el CEO del bot.",
+                    title: "© 2025–2025 Isagi Project",
+                    body: "Contacta con el owner del bot.",
                     thumbnailUrl: "https://files.catbox.moe/12zb63.jpg",
                     mediaType: 1,
                     renderLargerThumbnail: true,
