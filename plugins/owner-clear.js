f32caf60eb2f39942621d4a32572742c6a0f9500
𝐅𝐞𝐝𝐞 𝐔𝐜𝐡𝐢𝐡𝐚 <federicoxyzz@gmail.com>
2025-11-30 09:54:29 -0300

    Update owner-clear.js

--- plugins/owner-clear.js
+++ plugins/owner-clear.js
@@ -1,19 +1,37 @@
-for (const dirname of tmpPaths) {
-const files = readdirSync(dirname)
-for (const file of files) {
-const fullPath = join(dirname, file)
-const stats = statSync(fullPath)
-if (stats.isDirectory()) continue
-unlinkSync(fullPath)
-totalDeleted++
-}}
-await m.react('✔️')
-conn.reply(m.chat, `🪷 *Listo, se eliminaron ${totalDeleted} archivos de las carpetas temporales*.`, m)
-break
-}}} catch (err) {
-await m.react('✖️')
-await conn.reply(m.chat, `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${err.message}`, m)
-}}
+import { readdirSync, statSync, unlinkSync} from 'fs'
+import { join} from 'path'
+
+const handler = async (m, { conn, usedPrefix}) => {
+  try {
+    const tmpPaths = ['./tmp']
+    let totalDeleted = 0
+
+    for (const dirname of tmpPaths) {
+      const files = readdirSync(dirname)
+      for (const file of files) {
+        const fullPath = join(dirname, file)
+        const stats = statSync(fullPath)
+        if (stats.isDirectory()) continue
+        unlinkSync(fullPath)
+        totalDeleted++
+}
+}
+
+    await m.react('✔️')
+    await conn.reply(
+      m.chat,
+      `🪷 *Listo, se eliminaron ${totalDeleted} archivos de las carpetas temporales*.`,
+      m
+)
+} catch (err) {
+    await m.react('✖️')
+    await conn.reply(
+      m.chat,
+      `⚠︎ Se ha producido un problema.\n> Usa *${usedPrefix}report* para informarlo.\n\n${err.message}`,
+      m
+)
+}
+}
 
 handler.help = ['delai', 'dsowner', 'cleartmp', 'vaciartmp']
 handler.tags = ['owner']
